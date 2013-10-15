80cc5584cb5505c61a9f98e6dc07cb9b10c4958a
Johann Barbie <extraterrestrialintelligence@gmail.com>
2013-10-15 14:03:48 +0900

    lifter jackson version to latest through custom provider

--- RestNucleusBoilerplate/src/main/java/org/restnucleus/RestNucleusApplication.java
+++ RestNucleusBoilerplate/src/main/java/org/restnucleus/RestNucleusApplication.java
@@ -3,17 +3,20 @@ package org.restnucleus;
 import javax.inject.Inject;
 
 import org.glassfish.hk2.api.ServiceLocator;
-import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 public class RestNucleusApplication extends ResourceConfig {
 
     @Inject
     public RestNucleusApplication(ServiceLocator serviceLocator) {
         // Set package to look for resources in
-        packages("org.restnucleus.resources","org.glassfish.jersey.examples.jackson");
+        packages("org.restnucleus.resources");
 
         System.out.println("Registering injectables...");
 
@@ -21,6 +24,13 @@ public class RestNucleusApplication extends ResourceConfig {
 
         GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
         guiceBridge.bridgeGuiceInjector(ServletConfig.injector);
-        this.register(JacksonFeature.class);
+        // create custom ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        // create JsonProvider to provide custom ObjectMapper
+        JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+        provider.setMapper(mapper);
+        this.register(provider);
     }
 }
